2339b1db3a329d878cc78138d33ba433eb0edcac
RosaMonro <rosamaria.monro@gmail.com>
2023-05-13 22:49:33 +0200

    fin

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,40 @@ import Square from './Components/Square'
 
 
 export default function Board () {
-const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true)
+  const [squares, setSquares] = useState(Array(9).fill(null));
 
 function handleClick(i) {
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
   const nextSquares = squares.slice();
-  nextSquares[i] = "X";
-  setSquares(nextSquares);
+    if (xIsNext) {
+      nextSquares[i] = "✖";
+    } else {
+      nextSquares[i] = "❤";
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
 }
+
+  const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+      status = "Ganador: " + winner;
+    } else {
+      status = "Siguiente jugador: " + (xIsNext ? "✖" : "❤");
+    }
 
 
   return <>
     <h1>Tres en raya</h1>
-    
+
+    <div className="board">
+
+    <div className="status">{status}</div>
+
     <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -32,5 +54,27 @@ function handleClick(i) {
         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
+
+    </div>
   </> 
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
